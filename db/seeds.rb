f783255502b8c1c89a066124f2e0a7b4e8eb4486
Henry20161020 <henryli76@gmail.com>
2018-01-06 17:06:25 -0500

    4.2 update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: "APICS CPIM Tutoring", description: 'APICS stands for American Production and Inventory Control Society"; CSCP stands for "Certified Supply Chain Professional"')
 Product.create(name: "APICS CSCP Tutoring", description: 'APICS stands for American Production and Inventory Control Society"; CSCP stands for "Certified Supply Chain Professional"')
