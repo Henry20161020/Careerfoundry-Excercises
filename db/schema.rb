983e69688a421d763714765b3b10efe2dfa6b779
Henry20161020 <henryli76@gmail.com>
2018-01-09 21:15:49 -0500

    4.2 add colour column to product database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104021827) do
+ActiveRecord::Schema.define(version: 20180110021416) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180104021827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.string "colour"
   end
 
 end
