0e8789257da857c996a9461dd734812cc4aaeefc
Henry Li <Henryli76@gmail.com>
2018-01-30 19:00:46 +0000

    5.11 profile added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
          to: 'henryli76@gmail.com',
          subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Saware"
+    mail(to: user.email,
+         subject: "Welcome to #{@appname}!")
+  end
 end
