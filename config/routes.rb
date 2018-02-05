44dec0a7781362292734720f13f02c662684311d
Henry Li <Henryli76@gmail.com>
2018-02-05 02:28:15 +0000

    6.6 test data prepared

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
   get 'simple_pages/thank_you'
   post 'simple_pages/thank_you'
+  post 'payments/create'
 
   root 'simple_pages#landing_page'
 
